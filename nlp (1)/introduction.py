8d00b2c28fbb493f336f5cbab60c3547bd559a6c
Vichitra Saini <vichitrasaini10@gmail.com>
2026-09-21 22:56:38 +0530

    Add LSTM and RNN project

---
+++ nlp (1)/introduction.py
@@ -0,0 +1,89 @@
+## NLP (Natural Processing Language)
+## Natural Language Processing or NLP is used by Machines to Understand , Analyse , Manipulate,  & intepret Human Language
+## NLP Helps Computer to understand what human speaks and writes and respond meaningfully
+
+
+## NLP PipeLine
+## In NLP PipeLine , we get to see a thinking framework to how a approach a NLP Problem Statement
+
+
+## It consists of the steps as: 
+## 1)  Data Accquisation
+## 2)  Text Preprocessing (Text-Cleanup, Basic-Preprocessing, Advanced Preprocessing)
+## 3)  Feature Engineering 
+## 4)  Deployment
+## 5)  Monitering
+## 6)  Model Update
+
+
+## NLP Tasks Approach & Challanges
+
+  ## Common NLP Task :
+
+     ## Text/Documents Classification
+     ## Sentiments Analysis
+     ## Information Reterival
+     ## Language Detection
+     ## Next Word Prediction
+     ## Chatbot or ChatGpt
+     ## Text Summarization
+     ## Topic Modelling
+     ## Spell Check and Grammer Correction
+     ## Sppech to Text Model
+
+## Approach for NLP
+   
+   ## 1) Heuristic Approach
+   ## 2) Machine Learning Approach
+   ## 3) Deep Learning Approach
+
+
+    ## Heuristic Approach : if as per the Rule  Certained Condition is met then in that case we can make a conclusion our side or we can take further step (Condition Based Approach)
+
+    
+    ## for exp : <h1 >My name is Vichitra </h1> ---> All html tags wants to removed
+
+    ## It is a short cut way of finding the solution of the given problem & it is a rule based way of finding solution.
+    
+    ## Advantage of Heuristic
+      ## These are quick approach as they find the solution to the problems quickly
+      ## We use the in today problem as well  
+
+
+   ## Machine Learning Approach: Read from the previous Module
+   ## Deep Learning Approach: Used for Huge amount of data , Sequence maintain for data  
+
+      ## Deep Learning Algo:
+      ## RNN: Recurrent Netural Network
+      ## LSTM : Long short Term Memory  
+      ## GRU : Gated Recurrent Units
+      ## Bidirectional LSTM:  
+      ## Transformers ---> GPT ---> Generative Pre Trained
+
+
+## Challanges in NLP
+    
+    ## Humans Understand Context only
+    ## Contextual Words
+    ## Slangs
+    ## Synonyms 
+
+## Getting started with pipiline
+
+## 1) Data Acqusition:
+
+      ## NLP Application ----> Sentiment Analysis
+      
+      ## I need to train my model with the data so that it makes accurate - prediction
+   
+
+
+## Feature Engineering
+## Just because our end model is going to be an ML or DL model, we cannot directly feed raw data into it. Feature engineering helps transform, select, or create meaningful features so the model can learn patterns more effectively.
+
+
+
+## Use of Methods
+## Heuristic Method: Amount of data is very less 
+
+ 
